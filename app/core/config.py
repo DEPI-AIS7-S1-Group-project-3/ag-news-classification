ca72ff7aa0477d76141b7b7b2b8ae308b422bac1
amrkhaled100 <amrkhalednoser327@example.com>
2026-09-18 00:17:56 +0300

    Update project name in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,14 @@
 import os
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Invoice Intelligence Pipeline"
+    PROJECT_NAME: str = "AG News Text Classification Pipeline"
     API_V1_STR: str = "/api/v1"
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
