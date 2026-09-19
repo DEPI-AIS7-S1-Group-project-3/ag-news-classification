01dd8628fe523f89042a229554daa68fcec21415
Mo Elbadawy <mohamed.asaad.elbadawy@gmail.com>
2026-09-20 01:27:55 +0300

    feat: add frontend

--- app/main.py
+++ app/main.py
@@ -4,7 +4,16 @@ from app.api.routes_news import router as news_router
 from app.core.logging import setup_logging
 setup_logging()
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="AG News Classification API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include News Classification Router
 app.include_router(news_router, prefix=settings.API_V1_STR, tags=["News Classification"])
